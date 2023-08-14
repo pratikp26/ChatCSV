faf2d40023dfb49686a9c00eae4c98d930d5bd33
Pratik <pratik.26699@gmail.com>
2023-08-14 15:26:58 +0530

    Basic GUI done.

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+import streamlit as st
+
 def main():
-    #code here
+    st.set_page_config(page_title="ChatCSV 📊")
+    st.header("Talk to your CSV 💬")
+
+    file = st.file_uploader("Upload your CSV file below", type="csv")
 
 if __name__ == '__main__':
     main()
